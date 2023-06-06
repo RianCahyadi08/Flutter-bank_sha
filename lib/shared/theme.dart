217c7483359216d6ade9b95de6334a8d4f20ba51
RyanCahyadi <ryancahyadi11@gmail.com>
2023-06-07 06:13:22 +0700

    Create custom theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = Color(0xffFFFFFF);
+Color blackColor = Color(0xff14193F);
+Color greyColor = Color(0xffA4A8AE);
+Color lightBackgroundColor = Color(0xffF6F8FB);
+Color blackBackgroundColor = Color(0xff020518);
+Color blueColor = Color(0xff53C1F9);
+Color purpleColor = Color(0xff5142E6);
+Color greenColor = Color(0xff22B07D);
+Color numberBackgroundColor = Color(0xff1A1D2E);
+
+TextStyle titleOnBoarding =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 20, color: blackColor);
