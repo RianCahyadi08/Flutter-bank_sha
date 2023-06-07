d63fba706e817714c504fa9c10fcada6c3841a76
RyanCahyadi <ryancahyadi11@gmail.com>
2023-06-07 22:30:46 +0700

    Create splash screen" "

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -10,6 +10,7 @@ Color blueColor = Color(0xff53C1F9);
 Color purpleColor = Color(0xff5142E6);
 Color greenColor = Color(0xff22B07D);
 Color numberBackgroundColor = Color(0xff1A1D2E);
+Color darkBackgroundColor = Color(0xff020518);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
