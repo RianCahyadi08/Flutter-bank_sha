bb1f46531e24039ba2c6568524012e6347c9cafe
RyanCahyadi <ryancahyadi11@gmail.com>
2023-06-25 20:59:46 +0700

    Create widget transfer result user item

--- lib/views/screens/transfer_choose_user.dart
+++ lib/views/screens/transfer_choose_user.dart
@@ -1,6 +1,8 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/views/widgets/buttons.dart';
 import 'package:bank_sha/views/widgets/forms.dart';
 import 'package:bank_sha/views/widgets/transfer_recent_users_items.dart';
+import 'package:bank_sha/views/widgets/transfer_result_user_items.dart';
 import 'package:flutter/material.dart';
 
 class TransferChooseUser extends StatelessWidget {
@@ -40,7 +42,18 @@ class TransferChooseUser extends StatelessWidget {
             title: 'by Username',
             isShowTitle: false,
           ),
-          buildRecentUsers()
+          // buildRecentUsers(),
+          buildResultUsers(),
+          const SizedBox(
+            height: 274,
+          ),
+          CustomFilledButton(
+            title: 'Continue',
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
         ],
       ),
     );
@@ -83,6 +96,43 @@ class TransferChooseUser extends StatelessWidget {
   }
 
   Widget buildResultUsers() {
-    return Container();
+    return Container(
+      margin: EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Result',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          Wrap(
+            spacing: 17,
+            runSpacing: 17,
+            children: [
+              TransferResultUserItems(
+                imgUrl: 'assets/images/img_result1.png',
+                name: 'Yonna Jie',
+                tag: 'yoenna',
+                isVerified: true,
+              ),
+              TransferResultUserItems(
+                imgUrl: 'assets/images/img_result2.png',
+                name: 'Yonne Ka',
+                tag: 'yoenyu',
+                isSelected: true,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/transfer_result_user_items.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class TransferResultUserItems extends StatelessWidget {
+  final String imgUrl;
+  final String name;
+  final String tag;
+  final bool isVerified;
+  final bool isSelected;
+
+  const TransferResultUserItems({
+    Key? key,
+    required this.imgUrl,
+    required this.name,
+    required this.tag,
+    this.isVerified = false,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 14,
+        vertical: 22,
+      ),
+      width: 155,
+      height: 175,
+      decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: isSelected ? blueColor : whiteColor,
+            width: 2,
+          )),
+      child: Column(
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage(imgUrl))),
+            child: isVerified
+                ? Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 16,
+                      height: 16,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: whiteColor,
+                      ),
+                      child: Icon(
+                        Icons.check_circle,
+                        size: 14,
+                        color: greenColor,
+                      ),
+                    ),
+                  )
+                : null,
+          ),
+          SizedBox(
+            height: 13,
+          ),
+          Text(
+            name,
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          const SizedBox(
+            height: 2,
+          ),
+          Text(
+            '@$tag',
+            style: grayTextStyle.copyWith(
+              fontSize: 12,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
