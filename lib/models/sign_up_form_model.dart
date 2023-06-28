db52973e6513b0a591add4f256c02b5ce8eab2f8
RyanCahyadi <ryancahyadi11@gmail.com>
2023-06-28 12:57:56 +0700

    Create model for sign up form

---
+++ lib/models/sign_up_form_model.dart
@@ -0,0 +1,42 @@
+class SignUpFormModel {
+  final String? name;
+  final String? email;
+  final String? password;
+  final String? pin;
+  final String? profilepicture;
+  final String? ktp;
+
+  SignUpFormModel({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.pin,
+    this.profilepicture,
+    this.ktp,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'pin': pin,
+      'profile_picture': profilepicture,
+      'ktp': ktp,
+    };
+  }
+
+  SignUpFormModel copyWith({
+    String? pin,
+    String? proficePicture,
+    String? ktp,
+  }) =>
+      SignUpFormModel(
+        name: name,
+        email: email,
+        password: password,
+        pin: pin ?? this.pin,
+        profilepicture: profilepicture ?? this.profilepicture,
+        ktp: ktp ?? this.ktp,
+      );
+}
